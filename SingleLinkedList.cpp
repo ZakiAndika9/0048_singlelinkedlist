c09ff48b1c9c0606f534884e2087ba269e49d940
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-05-08 20:47:11 +0700

    commit 3

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -8,3 +8,36 @@ class Node
 	int noMhs;
 	Node *next;
 };
+
+class LinkedList
+{
+	Node *START;
+
+	public:
+	LinkedList()
+	{
+		START = NULL;
+	}
+
+	void addNode()
+	{
+		int nim;
+		cout <<"\nMasukkan Nomor Mahasiswa: ";
+		cin >> nim;
+
+		Node *nodeBaru = new Node;
+		nodeBaru->noMhs = nim;
+
+		if (START ==  NULL || nim <= START->noMhs)
+		{
+			if ((START != NULL) && (nim == START->noMhs))
+			{
+				cout << "\nDupllikasi noMhs tidak diijinkan\n";
+				return;
+			}
+			nodeBaru->next = START;
+			START = nodeBaru;
+			return;
+		}
+	}
+};
